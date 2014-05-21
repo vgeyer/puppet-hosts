95b2e5ff9ec5415059265d9adc4d99a5775ce3d4
Victor J. C. Geyer <victorgeyer@ciscaja.com>
2014-05-21 17:15:04 +0200

    Added module logic.

--- README.markdown
+++ README.markdown
@@ -7,10 +7,11 @@ Manage (dualstack-)hosts in /etc/hosts .
 
 ## Usage
 Various alt hostnames my be specified. IP Addresses are validated. At least a IPv4 OR IPv6 address must be specified.
-
+```puppet
 class { 'hosts':
     hosts => {
         'sidious.local' => {alt =>['sidious', 'puppet'], ipv4 => '192.168.2.1, ipv6 => 'fdc9:7739:4b55:da6:0:0:0:2'},
-        'vader.local' => {alt =>['vader'], ipv4 => '192.168.2.2', ipv6 => 'fdc9:7739:4b55:da6:0:0:0:10']}
+        'vader.local' => {alt =>['vader'], ipv4 => '192.168.2.2', ipv6 => 'fdc9:7739:4b55:da6:0:0:0:10'}
     }
 }
+```
---
+++ manifests/init.pp
@@ -0,0 +1,24 @@
+# Class: hosts
+#
+# Due restrictions of puppet as structural language it not possible to do this as type.
+# The hash is currently not validated, so take care ;-)
+#
+# Example:
+#  class{ 'hosts':
+#    hosts => {
+#      'sidious.srv1.tschnet.de' => {alt =>['sidious'], ipv4 => '144.76.42.151', ipv6 => '2a01:4f8:191:439a:0:0:0:2'},
+#      'vader.srv1.tschnet.de' => {alt =>['vader'], ipv4 => '144.76.133.212', ipv6 => '2a01:4f8:191:439a:0:0:0:10']}
+#    }
+#  }
+
+class hosts (
+  $hosts = undef
+) {
+  file { '/etc/hosts':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content  => template('hosts/etc/hosts.erb'),
+  }
+}
